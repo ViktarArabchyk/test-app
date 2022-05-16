b56e00dcbd8b5acd52cac10ebcbcc620782e2804
Viktar <viktar.arabchyk@itrexgroup.com>
2022-05-16 11:38:41 +0400

    Applyed stylelint code formatting. Class names converted to kebab-case

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,18 @@ import './App.css';
 
 function App() {
 	return (
-		<div className="App">
-			<header className="App-header">
+		<div className="app">
+			<header className="app-header">
 				<img
 					src={logo}
-					className="App-logo"
+					className="app-logo"
 					alt="logo"
 				/>
 				<p>
 					Edit <code>src/App.tsx</code> and save to reload.
 				</p>
 				<a
-					className="App-link"
+					className="app-link"
 					href="https://reactjs.org"
 					target="_blank"
 					rel="noopener noreferrer"
